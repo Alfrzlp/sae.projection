9b6ae8c159e2906d991623084ca1e34a9847fb8f
ridsonap <alfrzlp@gmail.com>
2025-02-15 18:33:08 +0800

    versi 0.1.1

--- R/projection.R
+++ R/projection.R
@@ -10,13 +10,14 @@
 #' @references
 #' \enumerate{
 #'  \item Kim, J. K., & Rao, J. N. (2012). Combining data from two independent surveys: a model-assisted approach. Biometrika, 99(1), 85-100.
-#'}
+#' }
 #'
 #' @param formula  An object of class formula that contains a description of the model to be fitted. The variables included in the formula must be contained in the \code{data_model} dan \code{data_proj}.
 #' @param id Column name specifying cluster ids from the largest level to the smallest level, where ~0 or ~1 represents a formula indicating the absence of clusters.
 #' @param weight Column name in data_proj representing the survey weight.
 #' @param strata 	Column name specifying strata, use NULL for no strata
 #' @param domain Column names in data_model and data_proj representing specific domains for which disaggregated data needs to be produced.
+#' @param fun A function taking a formula and survey design object as its first two arguments (default = "mean", "total", "varians").
 #' @param model The working model to be used in the projection estimator. Refer to the details for the available working models.
 #' @param data_model A data frame or a data frame extension (e.g., a tibble) representing the second survey, characterized by a much smaller sample, provides information on both the variable of interest and the auxiliary variables.
 #' @param data_proj A data frame or a data frame extension (e.g., a tibble) representing the first survey, characterized by a large sample that collects only auxiliary information or general-purpose variables.
@@ -27,6 +28,7 @@
 #' If "resamples", then tuning will be performed in parallel over resamples alone. Within each resample, the preprocessor (i.e. recipe or formula) is processed once, and is then reused across all models that need to be fit.
 #' If "everything", then tuning will be performed in parallel at two levels. An outer parallel loop will iterate over resamples. Additionally, an inner parallel loop will iterate over all unique combinations of preprocessor and model tuning parameters for that specific resample. This will result in the preprocessor being re-processed multiple times, but can be faster if that processing is extremely fast.
 #' @param seed A single value, interpreted as an integer
+#' @param est_y A logical value indicating whether to return the estimation of \code{y} in \code{data_model}. If \code{TRUE}, the estimation is returned; otherwise, it is not.
 #' @param ... Further argument to the \code{\link[survey]{svydesign}}.
 #'
 #' @return The function returns a list with the following objects (\code{model}, \code{prediction} and \code{df_result}):
@@ -39,9 +41,11 @@
 #'    * \code{rse_ypr} The Relative Standard Error (RSE) in percentage (%).
 #'
 #' @export
+#' @import bonsai
 #' @import tidymodels
-#' @importFrom dplyr %>%
-#' @importFrom rlang .data
+#' @import lightgbm
+#' @import ranger
+#'
 #'
 #' @details
 #' The available working models include:
@@ -64,6 +68,7 @@
 #' \dontrun{
 #' library(sae.projection)
 #' library(dplyr)
+#' library(bonsai)
 #'
 #' df_svy22_income <- df_svy22 %>% filter(!is.na(income))
 #' df_svy23_income <- df_svy23 %>% filter(!is.na(income))
@@ -71,23 +76,25 @@
 #' # Linear regression
 #' lm_proj <- projection(
 #'   income ~ age + sex + edu + disability,
-#'   id = 'PSU', weight = 'WEIGHT', strata = 'STRATA',
-#'   domain = c('PROV', 'REGENCY'),
+#'   id = "PSU", weight = "WEIGHT", strata = "STRATA",
+#'   domain = c("PROV", "REGENCY"),
 #'   model = linear_reg(),
 #'   data_model = df_svy22_income,
 #'   data_proj = df_svy23_income,
+#'   nest = TRUE
 #' )
 #'
 #' # Random forest regression with hyperparameter tunning
 #' rf_proj <- projection(
 #'   income ~ age + sex + edu + disability,
-#'   id = 'PSU', weight = 'WEIGHT', strata = 'STRATA',
-#'   domain = c('PROV', 'REGENCY'),
+#'   id = "PSU", weight = "WEIGHT", strata = "STRATA",
+#'   domain = c("PROV", "REGENCY"),
 #'   model = rand_forest(mtry = tune(), trees = tune(), min_n = tune()),
 #'   data_model = df_svy22_income,
 #'   data_proj = df_svy23_income,
 #'   kfold = 3,
-#'   grid = 30
+#'   grid = 10,
+#'   nest = TRUE
 #' )
 #'
 #' df_svy22_neet <- df_svy22 %>% filter(between(age, 15, 24))
@@ -96,42 +103,42 @@
 #' # Logistic regression
 #' lr_proj <- projection(
 #'   formula = neet ~ sex + edu + disability,
-#'   id = 'PSU',
-#'   weight = 'WEIGHT',
-#'   strata = 'STRATA',
-#'   domain = c('PROV', 'REGENCY'),
+#'   id = ~ PSU,
+#'   weight = ~ WEIGHT,
+#'   strata = ~ STRATA,
+#'   domain = ~ PROV + REGENCY,
 #'   model = logistic_reg(),
 #'   data_model = df_svy22_neet,
-#'   data_proj = df_svy23_neet
+#'   data_proj = df_svy23_neet,
+#'   nest = TRUE
 #' )
 #'
 #' # LightGBM regression with hyperparameter tunning
-#' library(bonsai)
-#' show_engines('boost_tree')
+#' show_engines("boost_tree")
 #' lgbm_model <- boost_tree(
-#'     mtry = tune(), trees = tune(), min_n = tune(),
-#'     tree_depth = tune(), learn_rate = tune(),
-#'    engine = 'lightgbm'
+#'   mtry = tune(), trees = tune(), min_n = tune(),
+#'   tree_depth = tune(), learn_rate = tune(),
+#'   engine = "lightgbm"
 #' )
 #'
 #' lgbm_proj <- projection(
 #'   formula = neet ~ sex + edu + disability,
-#'   id = 'PSU',
-#'   weight = 'WEIGHT',
-#'   strata = 'STRATA',
-#'   domain = c('PROV', 'REGENCY'),
+#'   id = "PSU",
+#'   weight = "WEIGHT",
+#'   strata = "STRATA",
+#'   domain = c("PROV", "REGENCY"),
 #'   model = lgbm_model,
 #'   data_model = df_svy22_neet,
 #'   data_proj = df_svy23_neet,
 #'   kfold = 3,
-#'   grid = 30
-#')
-#'}
+#'   grid = 10,
+#'   nest = TRUE
+#' )
+#' }
 #' @md
 projection <- function(
-    formula, id, weight, strata = NULL, domain, model,
-    data_model, data_proj, model_metric, kfold = 1, grid = 1, parallel_over = "resamples", seed = 1, ...
-  ){
+    formula, id, weight, strata = NULL, domain, fun = "mean", model,
+    data_model, data_proj, model_metric, kfold = 3, grid = 10, parallel_over = "resamples", seed = 1, est_y = FALSE, ...) {
 
   domain_chr <- domain
   id <- .check_variable(id, data_model, data_proj)
@@ -139,15 +146,30 @@ projection <- function(
   strata <- .check_variable(strata, data_model, data_proj)
   domain <- .check_variable(domain, data_model, data_proj)
 
+  if ((methods::is(domain_chr, 'formula'))) {
+    domain_chr <- colnames(stats::model.frame(domain, data_model, na.action = NULL))
+  }
+
   y_name <- as.character(rlang::f_lhs(formula))
   y <- data_model[[y_name]]
 
+  fun <- match.arg(fun, c("mean", "total", "varians"))
+  if (fun == "mean") {
+    FUN <- survey::svymean
+  } else if (fun == "total") {
+    FUN <- survey::svytotal
+  } else if (fun == "varians") {
+    FUN <- survey::svyvar
+  } else {
+    cli::cli_abort('fun must be "mean", "total", or "varians" not {fun}')
+  }
+
   # spesifikasi model -------------------------------------
-  if (class(y) %in% c('integer', 'numeric')) {
-    type <- 'regression'
+  if (class(y) %in% c("integer", "numeric")) {
+    type <- "regression"
     if (missing(model_metric)) model_metric <- yardstick::metric_set(yardstick::rmse)
-  }else{
-    type <- 'classification'
+  } else {
+    type <- "classification"
     if (missing(model_metric)) model_metric <- yardstick::metric_set(yardstick::f_meas)
   }
 
@@ -161,7 +183,7 @@ projection <- function(
     workflows::add_recipe(model_rec)
 
   # tunning model -------------------------------------
-  all_tune <- grepl('tune', as.character(sapply(model[['args']], rlang::quo_get_expr)))
+  all_tune <- grepl("tune", as.character(sapply(model[["args"]], rlang::quo_get_expr)))
   no_tune <- all(all_tune == FALSE)
 
   if (no_tune) {
@@ -169,9 +191,9 @@ projection <- function(
   } else {
     # set k-fold cv
     set.seed(seed)
-    if (type == 'regression') {
+    if (type == "regression") {
       model_fold <- rsample::vfold_cv(data_model, v = kfold)
-    }else if (type == 'classification') {
+    } else if (type == "classification") {
       model_fold <- rsample::vfold_cv(data_model, v = kfold, strata = y_name)
     }
 
@@ -195,7 +217,6 @@ projection <- function(
     final_fit <- parsnip::fit(final_fit, data_model)
   }
 
-
   final_model <- tune::extract_fit_engine(final_fit)
 
   # prediksi y pada data survei 1 -------------------------------------
@@ -204,60 +225,104 @@ projection <- function(
 
 
   # bias correction from survey 2 -------------------------------------
-  if (type == 'regression') {
+  if (type == "regression") {
     data_model$bias <- y - prediction
-  } else if (type == 'classification') {
+  } else if (type == "classification") {
     data_model$bias <- ifelse(y == prediction, 0, 1)
     data_proj$ypr <- as.numeric(levels(data_proj$ypr))[data_proj$ypr]
+
+    data_model[[y_name]] <- as.numeric(levels(y))[y]
   }
 
   svy2_design <- survey::svydesign(ids = id, weights = weight, strata = strata, data = data_model, ...)
   est_bias <- survey::svyby(
-    formula = ~ bias,
+    formula = ~bias,
     by = domain,
     design = svy2_design,
-    FUN = survey::svymean,
-    vartype = c('var')
+    FUN = FUN,
+    vartype = c("var")
   )
-  colnames(est_bias)[colnames(est_bias) == 'var'] <- 'var_bias'
+  colnames(est_bias)[colnames(est_bias) == "var"] <- "var_bias"
 
   # estimasi yhat pada data survei 1 ----------------------------------
-  svy1_design <- survey::svydesign(ids = id, weights = weight, strata = strata, data = data_proj, nest = TRUE, ...)
+  svy1_design <- survey::svydesign(ids = id, weights = weight, strata = strata, data = data_proj, ...)
   est_ypr <- survey::svyby(
-    formula = ~ ypr,
+    formula = ~ypr,
     by = domain,
     design = svy1_design,
-    FUN = survey::svymean,
-    vartype = c('var')
+    FUN = FUN,
+    vartype = c("var")
   )
-  colnames(est_ypr)[colnames(est_ypr) == 'var'] <- 'var_ypr'
+  colnames(est_ypr)[colnames(est_ypr) == "var"] <- "var_ypr"
 
   # hitung yhat pr, var yhatpr dan rse yhatpr -------------------------
   df_result <- dplyr::left_join(est_ypr, est_bias, by = domain_chr)
   ypr <- df_result$ypr + df_result$bias
-  var_ypr <-  df_result$var_ypr + df_result$var_bias
+  var_ypr <- df_result$var_ypr + df_result$var_bias
   rse_ypr <- sqrt(var_ypr) * 100 / ypr
 
   df_result$ypr <- ypr
   df_result$var_ypr <- var_ypr
   df_result$rse_ypr <- rse_ypr
-  df_result[, c('bias', 'var_bias')] <- NULL
+  df_result[, c("bias", "var_bias")] <- NULL
 
   all_result <- list(
     model = final_model,
-    prediction = data_proj$ypr,
-    df_result = df_result
+    prediction = data_proj$ypr
   )
+
+  if (est_y) {
+    est_y <- survey::svyby(
+      formula = stats::as.formula(paste0("~", y_name)),
+      by = domain,
+      design = svy2_design,
+      FUN = FUN,
+      vartype = c("var", "cvpct")
+    )
+    colnames(est_y)[colnames(est_y) == "cv%"] <- "rse"
+    all_result$est_y <- est_y
+  }
+
+  all_result$projection <- df_result
   return(all_result)
 }
 
 
+
+
+
+
+
+
 .check_variable <- function(variable, data_model, data_proj) {
-  if (methods::is(variable, "character")) {
+  if (is.null(variable)) {
+    return(variable)
+  }
+
+  if (methods::is(variable, "formula")) {
+    if (variable == ~1 | variable == ~0) return(variable)
+
+    tryCatch({
+      dat <- stats::model.frame(variable, data_model, na.action = NULL)
+      return(variable)
+    },
+    error = function(x){
+      cli::cli_abort('variable "{variable}" is not found in the data_model')
+    })
+
+    tryCatch({
+      dat <- stats::model.frame(variable, data_proj, na.action = NULL)
+      return(variable)
+    },
+    error = function(x){
+      cli::cli_abort('variable "{variable}" is not found in the data_proj')
+    })
+
+  }else if (methods::is(variable, "character")) {
     if (length(variable) > 1) {
       if (mean(variable %in% colnames(data_model)) == 1) {
         if (mean(variable %in% colnames(data_proj)) == 1) {
-          return(stats::as.formula(paste0('~', paste0(variable, collapse = ' + '))))
+          return(stats::as.formula(paste0("~", paste0(variable, collapse = " + "))))
         } else {
           cli::cli_abort('variable "{setdiff(variable, colnames(data_proj))}" is not found in the data_proj')
         }
@@ -266,7 +331,7 @@ projection <- function(
       }
     } else if (variable %in% colnames(data_model)) {
       if (variable %in% colnames(data_proj)) {
-        return(stats::as.formula(paste0('~', variable)))
+        return(stats::as.formula(paste0("~", variable)))
       } else {
         cli::cli_abort('variable "{variable}" is not found in the data_proj')
       }
@@ -274,6 +339,6 @@ projection <- function(
       cli::cli_abort('variable "{variable}" is not found in the data_model')
     }
   } else {
-    cli::cli_abort('variable "{variable}" must be character')
+    cli::cli_abort('variable "{variable}" must be character or formula')
   }
 }
--- README.md
+++ README.md
@@ -19,12 +19,16 @@ Ridson Al Farizal P <ridsonap@bps.go.id>
 
 ## Description
 
-The goal of **sae.projection** is to provide tools for performing
-**Small Area Estimation (SAE)** using projection-based methods. This
-approach is particularly effective for generating reliable domain-level
-estimates in situations where sample sizes are too small for direct
-estimation. By utilizing auxiliary information and statistical models,
-the package improves the precision and accuracy of small area estimates.
+The **sae.projection** package provides a robust tool for *small area
+estimation using a projection-based approach*. This method is
+particularly beneficial in scenarios involving two surveys, the first
+survey collects data solely on auxiliary variables, while the second,
+typically smaller survey, collects both the variables of interest and
+the auxiliary variables. The package constructs a working model to
+predict the variables of interest for each sample in the first survey.
+These predictions are then used to estimate relevant indicators for the
+desired domains. This condition overcomes the problem of estimation in a
+small area when only using the second survey data.
 
 ## Installation
 
@@ -42,75 +46,206 @@ This is a basic example which shows you how to solve a common problem:
 
 ``` r
 library(sae.projection)
+#> Loading required package: tidymodels
+#> ── Attaching packages ────────────────────────────────────── tidymodels 1.2.0 ──
+#> ✔ broom        1.0.7     ✔ recipes      1.1.0
+#> ✔ dials        1.3.0     ✔ rsample      1.2.1
+#> ✔ dplyr        1.1.4     ✔ tibble       3.2.1
+#> ✔ ggplot2      3.5.1     ✔ tidyr        1.3.1
+#> ✔ infer        1.0.7     ✔ tune         1.2.1
+#> ✔ modeldata    1.4.0     ✔ workflows    1.1.4
+#> ✔ parsnip      1.2.1     ✔ workflowsets 1.1.0
+#> ✔ purrr        1.0.2     ✔ yardstick    1.3.1
+#> ── Conflicts ───────────────────────────────────────── tidymodels_conflicts() ──
+#> ✖ purrr::discard() masks scales::discard()
+#> ✖ dplyr::filter()  masks stats::filter()
+#> ✖ dplyr::lag()     masks stats::lag()
+#> ✖ recipes::step()  masks stats::step()
+#> • Use suppressPackageStartupMessages() to eliminate package startup messages
 library(dplyr)
-#> Warning: package 'dplyr' was built under R version 4.2.3
-#> 
-#> Attaching package: 'dplyr'
-#> The following objects are masked from 'package:stats':
-#> 
-#>     filter, lag
-#> The following objects are masked from 'package:base':
-#> 
-#>     intersect, setdiff, setequal, union
 ```
 
-### Logistic Model
+## Regression
+
+### Data
 
 ``` r
-df_res <- projection_estimator(
-   formula = unemployment ~ edu + sex + age + head,
-   domain = c('prov', 'regency'),
-   weight = 'weight',
-   data_model = df_svy2,
-   data_proj = df_svy1,
-   model = 'logistic',
-   cutoff = 'optimum'
-)
-#> ℹ Getting the optimum cut-off...
-#> ✔ The optimum cut-off is 0.105987114406781 and 0.30650406504065
-#> Warning in predict_result > cutoff: longer object length is not a multiple of
-#> shorter object length
-#> # A tibble: 119 × 6
-#>     prov regency     n   yhat  var_yhat   rse
-#>    <int>   <int> <int>  <dbl>     <dbl> <dbl>
-#>  1    31       1   329 0.0633 0.000199  22.3 
-#>  2    31      71  1229 0.0750 0.0000586 10.2 
-#>  3    31      72  1368 0.0725 0.0000522  9.97
-#>  4    31      73   846 0.0800 0.0000837 11.4 
-#>  5    31      74  1265 0.109  0.0000714  7.74
-#>  6    31      75  1126 0.0941 0.0000633  8.46
-#>  7    32       1  1476 0.115  0.0000597  6.71
-#>  8    32       2  1224 0.0899 0.0000585  8.50
-#>  9    32       3  1248 0.0841 0.0000508  8.48
-#> 10    32       4  1353 0.0854 0.0000557  8.73
-#> # ℹ 109 more rows
+df_svy22_income <- df_svy22 %>% filter(!is.na(income))
+df_svy23_income <- df_svy23 %>% filter(!is.na(income))
 ```
 
-### Linear Model
+### Linear Regression Model
 
 ``` r
-df_svy2_new <- df_svy2 %>% dplyr::filter(!is.na(income))
-
-m2 <- projection_estimator(
-  formula = income ~ edu + sex + age + head,
-  domain = c('prov', 'regency'),
-  weight = 'weight',
-  data_model = df_svy2_new,
-  data_proj = df_svy1,
-  model = 'linear'
+lm_proj <- projection(
+   income ~ age + sex + edu + disability,
+   id = 'PSU', weight = 'WEIGHT', strata = 'STRATA',
+   domain = c('PROV', 'REGENCY'),
+   model = linear_reg(),
+   data_model = df_svy22_income,
+   data_proj = df_svy23_income,
 )
-#> # A tibble: 119 × 6
-#>     prov regency     n     yhat    var_yhat   rse
-#>    <int>   <int> <int>    <dbl>       <dbl> <dbl>
-#>  1    31       1   329 2579381. 4931305728.  2.72
-#>  2    31      71  1229 3202174. 1814357196.  1.33
-#>  3    31      72  1368 3158206. 1516683859.  1.23
-#>  4    31      73   846 2958848. 2221792017.  1.59
-#>  5    31      74  1265 2749878. 1210877276.  1.27
-#>  6    31      75  1126 2965990. 1785753160.  1.42
-#>  7    32       1  1476 2414944.  876222281.  1.23
-#>  8    32       2  1224 2075254.  772421710.  1.34
-#>  9    32       3  1248 1996901.  676199395.  1.30
-#> 10    32       4  1353 2567675. 1073105001.  1.28
-#> # ℹ 109 more rows
+
+lm_proj$df_result
+#>    PROV REGENCY     ypr      var_ypr   rse_ypr
+#> 1    35       1 1301174  10618881790  7.919617
+#> 2    35       2 1575921   8089890094  5.707383
+#> 3    35       3 1515173   8851908817  6.209496
+#> 4    35       4 2016174  13493709449  5.761527
+#> 5    35       5 1630143   6715811111  5.027170
+#> 6    35       6 1737991   7666303467  5.037853
+#> 7    35       7 2214592  20119175179  6.404887
+#> 8    35       8 1903982  18080432329  7.062225
+#> 9    35       9 1773319   4428235969  3.752567
+#> 10   35      10 2034861   4602423996  3.333946
+#> 11   35      11 1500750  26703134922 10.888621
+#> 12   35      12 1722463  10352480692  5.907074
+#> 13   35      13 2053568  24669804362  7.648456
+#> 14   35      14 2105472  10160099561  4.787397
+#> 15   35      15 3737909 144213198696 10.159535
+#> 16   35      16 2412471   8240112740  3.762743
+#> 17   35      17 2187732   7891875275  4.060655
+#> 18   35      18 2117781  21478193260  6.920187
+#> 19   35      19 1797575   7877467222  4.937490
+#> 20   35      20 2053399   7851002103  4.315084
+#> 21   35      21 1691834   4192575324  3.827214
+#> 22   35      22 1905703   7757174634  4.621646
+#> 23   35      23 2090962   6458363757  3.843395
+#> 24   35      24 2223585   9689490994  4.426869
+#> 25   35      25 3172802  11755846028  3.417307
+#> 26   35      26 1824850  15006851832  6.713016
+#> 27   35      27 1558080  25660267931 10.281133
+#> 28   35      28 1525220   9180681192  6.282101
+#> 29   35      29 1834459  19430440166  7.598595
+#> 30   35      71 2541951  43342967547  8.190156
+#> 31   35      72 2284458  10360590216  4.455630
+#> 32   35      73 2835409  20677060812  5.071410
+#> 33   35      74 2602627  56026780853  9.094647
+#> 34   35      75 2585689  22048185343  5.742620
+#> 35   35      76 2888925  14002070555  4.095999
+#> 36   35      77 2554860  21995898018  5.805021
+#> 37   35      78 4594493 300406206504 11.929351
+#> 38   35      79 2562641  10071273136  3.916106
+```
+
+### Random Forest Regression with Hyperparameter Tunning
+
+``` r
+rf_proj <- projection(
+  income ~ age + sex + edu + disability,
+  id = 'PSU', weight = 'WEIGHT', strata = 'STRATA',
+  domain = c('PROV', 'REGENCY'),
+  model = rand_forest(mtry = tune(), trees = tune(), min_n = tune()),
+  data_model = df_svy22_income,
+  data_proj = df_svy23_income,
+  kfold = 3,
+  grid = 20
+)
+
+rf_proj$df_result
+```
+
+## Classification
+
+### Data
+
+``` r
+df_svy22_neet <- df_svy22 %>% 
+  filter(between(age, 15, 24))
+df_svy23_neet <- df_svy23 %>% 
+  filter(between(age, 15, 24))
+```
+
+### Logistic Regression
+
+``` r
+lr_proj <- projection(
+  formula = neet ~ sex + edu + disability,
+  id = 'PSU',
+  weight = 'WEIGHT',
+  strata = 'STRATA',
+  domain = c('PROV', 'REGENCY'),
+  model = logistic_reg(),
+  data_model = df_svy22_neet,
+  data_proj = df_svy23_neet
+)
+
+lr_proj$df_result
+#>    PROV REGENCY       ypr      var_ypr   rse_ypr
+#> 1    35       1 0.1883741 0.0009101754 16.015535
+#> 2    35       2 0.1761453 0.0005962890 13.863006
+#> 3    35       3 0.2627529 0.0008963487 11.394387
+#> 4    35       4 0.1501824 0.0005408503 15.485294
+#> 5    35       5 0.1970353 0.0007401344 13.807382
+#> 6    35       6 0.2236771 0.0004727475  9.720599
+#> 7    35       7 0.2339609 0.0004360528  8.925373
+#> 8    35       8 0.3226574 0.0008149866  8.847767
+#> 9    35       9 0.2947512 0.0005243011  7.768458
+#> 10   35      10 0.2439578 0.0006033091 10.068281
+#> 11   35      11 0.2767459 0.0010785238 11.866799
+#> 12   35      12 0.2453357 0.0006228644 10.172695
+#> 13   35      13 0.3123293 0.0005482030  7.496490
+#> 14   35      14 0.3277912 0.0008762413  9.030557
+#> 15   35      15 0.1347073 0.0002041638 10.607138
+#> 16   35      16 0.2359613 0.0006320838 10.654830
+#> 17   35      17 0.2293048 0.0003759636  8.455897
+#> 18   35      18 0.1947305 0.0006598068 13.190899
+#> 19   35      19 0.2014381 0.0008690814 14.634864
+#> 20   35      20 0.1471380 0.0006121382 16.815121
+#> 21   35      21 0.1766580 0.0010066641 17.960110
+#> 22   35      22 0.2105672 0.0006411531 12.025142
+#> 23   35      23 0.2568354 0.0007179900 10.432882
+#> 24   35      24 0.2224894 0.0004518400  9.553950
+#> 25   35      25 0.2033393 0.0006942281 12.957752
+#> 26   35      26 0.2489649 0.0011293225 13.498045
+#> 27   35      27 0.3110092 0.0018619150 13.874160
+#> 28   35      28 0.1703990 0.0004611499 12.602423
+#> 29   35      29 0.2939050 0.0012088401 11.829802
+#> 30   35      71 0.1395280 0.0006020211 17.585090
+#> 31   35      72 0.1424188 0.0005480416 16.437644
+#> 32   35      73 0.1783830 0.0004321919 11.654264
+#> 33   35      74 0.2186362 0.0006895557 12.010541
+#> 34   35      75 0.1700568 0.0005773959 14.130020
+#> 35   35      76 0.1880439 0.0006208718 13.250790
+#> 36   35      77 0.1415133 0.0005442635 16.485696
+#> 37   35      78 0.1759323 0.0002766692  9.454418
+#> 38   35      79 0.1786988 0.0004931984 12.427652
+```
+
+### LightGBM with Hyperparameter Tunning
+
+``` r
+library(bonsai)
+show_engines('boost_tree')
+#> # A tibble: 7 × 2
+#>   engine   mode          
+#>   <chr>    <chr>         
+#> 1 xgboost  classification
+#> 2 xgboost  regression    
+#> 3 C5.0     classification
+#> 4 spark    classification
+#> 5 spark    regression    
+#> 6 lightgbm regression    
+#> 7 lightgbm classification
+lgbm_model <- boost_tree(
+  mtry = tune(), trees = tune(), min_n = tune(), tree_depth = tune(), learn_rate = tune(),
+  engine = 'lightgbm'
+)
+```
+
+``` r
+lgbm_proj <- projection(
+  formula = neet ~ sex + edu + disability,
+  id = 'PSU',
+  weight = 'WEIGHT',
+  strata = 'STRATA',
+  domain = c('PROV', 'REGENCY'),
+  model = lgbm_model,
+  data_model = df_svy22_neet,
+  data_proj = df_svy23_neet,
+  kfold = 3, 
+  grid = 20
+)
+
+lgbm_proj$df_result
 ```
